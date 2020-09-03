62318aee83fe8417a4a18e35e29e6c88bcd57b95
ahmadsyaikhu <ahmadsyaikhu99@gmail.com>
2020-09-04 02:04:55 +0700

    update tugas 9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="form">
+        <h1>Form pembelian Buah</h1>
+        <table>
+          <tr>
+            <th>
+              <label style={{ fontWeight: "bold" }}>Nama Pelanggan : </label>
+            </th>
+            <th>
+              <input type="teks" />
+            </th>
+          </tr>
+          <tr>
+            <th>
+              <label style={{ fontWeight: "bold" }}>Daftar Item : </label>
+            </th>
+            <td>
+              <input type="checkbox" />
+              <label>Semangka</label>
+              <br />
+              <input type="checkbox" />
+              <label>Jeruk</label>
+              <br />
+              <input type="checkbox" />
+              <label>Nanas</label>
+              <br />
+              <input type="checkbox" />
+              <label>Salak</label>
+              <br />
+              <input type="checkbox" />
+              <label>Anggur</label>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="submit" value="Kirim"></input>
+            </td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
